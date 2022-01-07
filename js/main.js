afb16a4602bf4375d7d844c20159b93c8bccedf7
Fabion Dautaj <fabion.dautaj@maggioli.it>
2022-01-07 21:38:33 +0100

    fix footer

--- js/main.js
+++ js/main.js
@@ -3,3 +3,21 @@ document.addEventListener("DOMContentLoaded", function(){
    navbar_height = document.querySelector('.navbar.fixed-top').offsetHeight;
    document.body.style.paddingTop = navbar_height + 'px';
 });
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+   footer_cols = document.querySelector("footer").querySelectorAll(".d-flex")
+   footer_cols.forEach(c => {
+      c.classList.remove("flex-column")
+      c.classList.add("flex-row")
+      c.classList.add("my-3")
+   })
+}
+
+if (window.matchMedia("(min-width: 768px)").matches) {
+   footer_cols = document.querySelector("footer").querySelectorAll(".d-flex")
+   footer_cols.forEach(c => {
+      c.classList.remove("flex-row")
+      c.classList.remove("my-3")
+      c.classList.add("flex-column")
+   })
+}
